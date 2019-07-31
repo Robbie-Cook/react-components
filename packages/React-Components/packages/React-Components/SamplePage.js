2678b51adbcd8b5a23f9c3c88e638e8f4622d0a1
Robbie Cook <robbieck7@gmail.com>
2019-07-31 20:37:11 +1200

    Merge branch 'master' into feature/lerna

--- packages/React-Components/packages/React-Components/SamplePage.js
+++ packages/React-Components/packages/React-Components/SamplePage.js
@@ -1,29 +1,19 @@
-import React, { Component } from 'react';
-import WebPage from '../components/Page';
-import { Heading } from '../components/Typography';
+import React, { Component } from "react"
+import WebPage from "../components/Page"
+import { Heading } from "./typography"
 import { Col, Row, LayoutWrapper } from "../components/Layout"
 
 // Main Page component
-class Sample extends Component {
-    render() {
-      return (
-  
-        <WebPage>
-          <LayoutWrapper>
-            <Heading type="h1">Sample</Heading>
-            <Row>
-              <Col>
-                
-              </Col>
-              <Col>
-
-              </Col>
-            </Row>
-          </LayoutWrapper>
-        </WebPage>
-      );
-    }
-  }
-  
-  export default Sample;
-  
+export default function Sample(props) {
+  return (
+    <WebPage>
+      <LayoutWrapper>
+        <Heading type="h1">Sample</Heading>
+        <Row>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+      </LayoutWrapper>
+    </WebPage>
+  )
+}
