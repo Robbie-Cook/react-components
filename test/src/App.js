4d174e404a348364b00bd5ac90b8ad68f00d31d1
RobbieCook <robbieck7@gmail.com>
2019-08-15 02:22:36 +1200

    Starting storyboard

--- test/src/App.js
+++ test/src/App.js
@@ -2,25 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {Heading} from "@robbie-cook/react-components"
+import { Heading } from "@robbie-cook/react-components"
 
 function App() {
   return (
     <div className="App">
-      <Heading>Test heading</Heading>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Heading>Storyboard for React Components</Heading>
       </header>
     </div>
   );
