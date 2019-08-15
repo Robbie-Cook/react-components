2dd34e43ed84bb069932e5b2c9380244c8ca295d
RobbieCook <robbieck7@gmail.com>
2019-08-15 22:52:19 +1200

    Fixed instance accidentally used instead of static class in context
    Maybe we could switch this to instance with modifiable parameters?

--- src/themes/ThemeContext.js
+++ src/themes/ThemeContext.js
@@ -2,6 +2,6 @@ import React from "react"
 import { Colors } from "../themes"
 
 // Provides an easy way to adjust themes for an individual project
-const ThemeContext = React.createContext(new Colors())
+const ThemeContext = React.createContext(Colors)
 
 export default ThemeContext
