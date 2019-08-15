d31e3375be6cbba886cce0e41b91c8c109930847
RobbieCook <robbieck7@gmail.com>
2019-08-15 20:02:00 +1200

    Moved Sizes to themes

--- src/layout/index.js
+++ src/layout/index.js
@@ -2,6 +2,5 @@ export { BorderBox } from './Boxes';
 export { Col, Row, LayoutWrapper } from "./Layout"
 export { default as WebPage } from "./Page"
 export { default as Section } from "./Section"
-export { default as Sizes } from "./Sizes"
 export { MobileView, ViewSizes } from "./Views"
 export { Container } from "./Wrappers"
