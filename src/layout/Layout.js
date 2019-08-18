1f6ad4b99aa4500567e71cfba2d7cdae03c5b07d
Robbie Cook <robbieck7@gmail.com>
2019-08-19 00:26:18 +1200

    Fixed issue with Col component

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,18 +2,18 @@
  * A component which defines Row and Col for layout
  */
 
-import styled from "styled-components"
-import { DefaultTheme } from "../themes"
+import styled from "styled-components";
+import { DefaultTheme } from "../themes";
 
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import { MobileView } from "./Views"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { MobileView } from "./Views";
 
 const LayoutWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-`
+`;
 
 const Row = styled.div`
   display: flex;
@@ -23,28 +23,29 @@ const Row = styled.div`
   ${new MobileView(`
     flex-direction: column;
   `)}
-`
+`;
 
-class Col extends Component {
+/**
+ * A column on a page
+ */
+function Col(props) {
   propTypes = {
-    width: PropTypes.string, // Given as a string,
+    width: PropTypes.string // Given as a string,
     // not as a number, e.g. 50%
-  }
+  };
 
-  render() {
-    Col = styled.div`
-      display: flex;
-      flex-direction: column;
-      margin: 0 40px 20px 0;
-      width: ${this.props.width};
+  const StyleCol = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 0 40px 20px 0;
+    width: ${props.width};
 
-      ${new MobileView(`
+    ${new MobileView(`
         width: 100%;
       `)}
-    `
+  `;
 
-    return <Col>{this.props.children}</Col>
-  }
+  return <StyleCol>{props.children}</StyleCol>;
 }
 
-export { Col, Row, LayoutWrapper }
+export { Col, Row, LayoutWrapper };
