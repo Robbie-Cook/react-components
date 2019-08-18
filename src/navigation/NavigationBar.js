64e921cd484b7767a8a7da3459bf3561b124516a
Robbie Cook <robbieck7@gmail.com>
2019-08-18 21:01:37 +1200

    Starting migrating Navigation Bar for active use

--- src/navigation/NavigationBar.js
+++ src/navigation/NavigationBar.js
@@ -1,47 +1,71 @@
-import AppBar from "@material-ui/core/AppBar";
-import { withStyles } from "@material-ui/core/styles";
-import Tabs from "@material-ui/core/Tabs";
-import Toolbar from "@material-ui/core/Toolbar";
+import React, { useContext, useState, useEffect } from "react";
+// import MobileNav from "../navigation/MobileNav";
+import { ThemeContext, DefaultTheme } from "../themes";
+import MyLink from "../utilities/MyLink";
 import { Transition } from "../animation";
 import styled from "styled-components";
-// import { MyAnilink } from "../animation";
-import { MobileView, ViewSizes } from "../layout/Views";
-// import MobileNav from "../navigation/MobileNav";
-import { DefaultTheme, Sizes } from "../themes";
-import { Spacer } from "../ui-components";
-import { Heading, Link, MyLink } from "../utilities";
+import { SiteContext } from "../utilities/SiteContext";
+import { Heading, Text } from "../utilities/Typography";
+import { Button } from "../ui-components/Button";
+import Spacer from "../ui-components/Spacer"
+import NavButton from "./NavButton"
+
+/** Represents a navbar element */
+function NavBar(props) {
+  const theme = useContext(ThemeContext).navbar;
+
+  const StyledNavBar = styled.div`
+    height: ${theme.height};
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    background-color: ${theme.backgroundColor};
+    padding-left: 40px;
+    padding-right: 40px;
+    box-sizing: border-box;
+  `;
+
+  const links = (() => {
+    let mylinks = [];
+    props.links.forEach(value => {
+      mylinks.push(<NavButton to={value.path} label={value.name} active={value.isActive()} />);
+    });
+    return mylinks;
+  })();
+
+  const Title = styled(Text)``;
+
+  return (
+    <StyledNavBar>
+      <Title>{props.title}</Title>
+      <Spacer width="30px" />
+      {links}
+    </StyledNavBar>
+  );
+}
+NavBar.defaultProps = {
+  title: "Testing title",
+  links: DefaultTheme.navbar.links
+};
 
 /**
  * Navigation bar UI component (version 2, without material-ui)
  */
-export default function NavigationBar() {
-  // Set up a loading state to trigger when component has painted
-  const [loaded, setLoaded] = setState(false);
+export default function NavigationBar(props) {
+  const [loaded, setLoaded] = useState(false);
   useEffect(() => {
     setLoaded(true);
   }, []);
 
-  const context = useContext(ThemeContext);
-
-  // TODO: turn this into a prop / theme
-  let navigationLinks = [new MyLink("Home", "/"), new MyLink("News", "/news")];
-  let heading = "Open Politics";
+  // Careful -- make sure that react is linked to parent when using npm link and useContext
+  const theme = useContext(ThemeContext).navbar;
+  const site = useContext(SiteContext);
 
   return (
-    <Bar navigationLinks={navigationLinks}>
-    </Bar>
+    <>
+      {/* <Transition loaded={loaded}> */}
+      <NavBar title={site.name} links={site.links} />
+      {/* </Transition> */}
+    </>
   );
-}
-
-/**
- * Represents a navigation bar
- * @param {*} props 
- */
-function Bar(props) {
-
-}
-Bar.defaultProps = {
-  navigationLinks: [new MyLink("Home", "/"), new MyLink("News", "/news")],
-  height: "60px",
-  backgroundColor: DefaultTheme.navbar.backgroundColor
 }
