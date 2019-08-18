8e8a8b7c2812cbf1c099a779696945751106d3ff
Robbie Cook <robbieck7@gmail.com>
2019-08-18 21:02:53 +1200

    Added comment to ThemeContext

--- src/themes/ThemeContext.js
+++ src/themes/ThemeContext.js
@@ -1,7 +1,9 @@
 import React from "react"
 import DefaultTheme from "../themes/DefaultTheme"
 
-// Provides an easy way to adjust themes for an individual project
+/** 
+ * Provides an easy way to adjust themes for an individual project
+ */
 const ThemeContext = React.createContext(DefaultTheme)
 
 export default ThemeContext
