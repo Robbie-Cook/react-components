d330353e8bfbd869eadbffd45cd4d047088fd196
Robbie Cook <robbieck7@gmail.com>
2019-08-25 21:50:32 +1200

    Migrated App.js from develop branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { Heading, Transition, ThemeContext, DefaultTheme } from "@robbie-cook/react-components";
+import { Button } from "./ui-components/Button"
+import { DefaultSiteContext, SiteContext } from "./utilities/SiteContext"
+import DefaultTheme from "./themes/DefaultTheme"
+import Dimensions from "./utilities/Dimensions"
+import {Heading, Link, Text } from "./utilities/Typography"
+import Page from  "./layout/Page"
+import ThemeContext from "./themes/ThemeContext"
+import Transition from "./animation/Transition"
 
+import React, { useEffect, useState } from "react";
+
+/**
+ * The main entry point for the app
+ */
 function App() {
+  // State updated when component painted
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 2000);
+  }, []);
+
+  new Dimensions();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SiteContext.Provider value={DefaultSiteContext}>
+      <ThemeContext.Provider value={DefaultTheme}>
+        <Page next={false}>
+          <Transition show={loaded} spinner={true}>
+            <Heading>My React storyboard</Heading>
+            <Text>
+              This is a test of the different components in{" "}
+              <Link href="https://www.npmjs.com/package/@robbie-cook/react-components">
+                @robbie-cook/react-components
+              </Link>
+            </Text>
+            <Button label="Testing button" />
+
+            <Heading type="h2">Testing loader</Heading>
+            <Transition show={false} spinner={true}>
+              <Text>Hidden text</Text>
+            </Transition>
+          </Transition>
+        </Page>
+      </ThemeContext.Provider>
+    </SiteContext.Provider>
   );
 }
 
