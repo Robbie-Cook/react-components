5914db21e12b81c33be64960fb57901c8e4a3ca5
Robbie Cook <robbieck7@gmail.com>
2019-11-30 15:13:58 +1300

    Added aliasing

--- webpack.js
+++ webpack.js
@@ -1,13 +1,23 @@
 // webpack.config.js
 const path = require('path');
+var nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: './index.js',
-  target: 'web',
+  target: 'node',
+  externals: [nodeExternals()],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     libraryTarget: 'commonjs',
+  },
+  resolve: {
+    alias: {
+      Redux: path.resolve(__dirname, 'src/redux/'),
+      Components: path.resolve(__dirname, 'src/components/'),
+      Helpers: path.resolve(__dirname, 'src/helpers/'),
+      Constants: path.resolve(__dirname, 'src/constants/'),
+    },
   },
   module: {
     rules: [
