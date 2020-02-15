aab12eb50f31ed9ac2a72fc112bbb4b05127b696
Robbie Cook <robbieck7@gmail.com>
2020-02-16 01:18:47 +1300

    Started fleshing out forms

---
+++ src/components/user/SignupForm.tsx
@@ -0,0 +1,56 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { useState, Fragment } from "react";
+import Input from "../input/Input";
+import Button from "../Button";
+import { useTheme } from "@robbie-cook/themer";
+import H3 from "../text/H3";
+import ContentBox from "../ContentBox";
+
+interface ISignupFormProps {
+  onSubmit: (email: string, password: string) => void;
+}
+
+/** The UI for a Signup form */
+const SignupForm: React.FC<ISignupFormProps> = props => {
+  const theme = useTheme();
+
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  return (
+    <ContentBox {...props}>
+      <H3
+        css={css`
+          text-transform: uppercase;
+          text-align: center;
+          margin-bottom: 40px;
+          min-width: 250px;
+        `}
+      >
+        Signup
+      </H3>
+      <Input
+        placeholder="Username"
+        column
+        onChange={(event: any) => setUsername(event.target.value)}
+      />
+      <Input
+        placeholder="Password"
+        column
+        type="password"
+        onChange={(event: any) => setPassword(event.target.value)}
+      />
+      <Button
+        css={css`
+          margin-top: 15px;
+        `}
+        onClick={() => props.onSubmit(username, password)}
+      >
+        Log in
+      </Button>
+    </ContentBox>
+  );
+};
+
+export default SignupForm;
--- src/components/user/UserDetailsForm.tsx
+++ src/components/user/UserDetailsForm.tsx
@@ -1,14 +1,21 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
-import H1 from "../text/H1";
+import H3 from "../text/H3";
+import Button from '../Button';
 
 export interface IUserDetailsFormProps {
+  photo?: string,
+  username?: string,
+  email?: string
+  onLogout?(): void;
 }
 
 export default function UserDetailsForm (props: IUserDetailsFormProps) {
   return (
     <div>
-      <H1>Hi, everybody</H1>
+      <H3>{props.username ? `Welcome, ${props.username}!` : `Welcome!`}</H3>
+      <p>Email: <span>{props.email}</span></p>
+      <Button onClick={props.onLogout}>Log out</Button>
     </div>
   );
 }
