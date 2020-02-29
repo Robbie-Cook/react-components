2b0a6ed575a2d68b583d39177bfdacfd03c04050
Robbie Cook <robbieck7@gmail.com>
2020-03-01 00:18:54 +1300

    IMPORTANT: Fixed build code

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,12 @@ import LoginForm from "./components/user/LoginForm";
 import UserDetailsForm from "./components/user/UserDetailsForm";
 import SignupForm from "./components/user/SignupForm";
 import ContentBox from "./components/ContentBox";
+import H1 from "./components/text/H1";
+import H2 from "./components/text/H2";
 
 export {
+  H1,
+  H2,
   Button,
   Input,
   DropdownMenu,
